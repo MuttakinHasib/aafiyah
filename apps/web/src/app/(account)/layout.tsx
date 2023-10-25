9ad2c2b0c1886f70fe03129735a7a276d90ad1fc
Muttakin Islam Hasib <muttakinislamhasib@gmail.com>
2023-10-26 03:01:57 +0600

    feat: :hammer: fix nav style

--- apps/web/src/app/(account)/layout.tsx
+++ apps/web/src/app/(account)/layout.tsx
@@ -5,7 +5,7 @@ const AccountLayout = (props: PropsWithChildren) => {
   return (
     <div className="bg-gray-100">
       <div className="container xl:max-w-6xl w-full py-16 grid lg:grid-cols-12 gap-8">
-        <div className="lg:col-span-3 shadow-header bg-white">
+        <div className="lg:col-span-3 shadow-header bg-white flex md:block overflow-auto">
           <AccountNavigation />
         </div>
         <div className="lg:col-span-9">{props.children}</div>
--- libs/web/ui/src/components/navigation/account.tsx
+++ libs/web/ui/src/components/navigation/account.tsx
@@ -10,13 +10,13 @@ export const AccountNavigation = () => {
     const pathname = usePathname()
   return (
     <React.Fragment>
-      <h4 className='py-5 px-6 text-xl font-medium'>Navigation</h4>
+      <h4 className='py-5 px-6 text-xl font-medium hidden md:block'>Navigation</h4>
       {
         NAVIGATION[1]?.children?.map(nav => (
-            <Link key={nav.label} className={cn('py-2 text-[15px] hover:bg-gray-100 px-6 block',{'font-medium shadow-[inset_3px_0_#e52727]': pathname === nav.path})} href={{pathname: nav.path}}>{nav.label}</Link>
+            <Link key={nav.label} className={cn('whitespace-nowrap py-2 text-[15px] hover:bg-gray-100 px-6 block',{'font-medium border-b-4 md:border-none border-brand md:shadow-[inset_3px_0_#e52727]': pathname === nav.path})} href={{pathname: nav.path}}>{nav.label}</Link>
         ))
       }
-      <div className='w-full border-b'/>
+      <div className='w-full border-b hidden md:block'/>
       <button title='Logout' type='button' className='py-2 outline-none w-full text-left text-[15px] hover:bg-gray-100 px-6 block'>Logout</button>
     </React.Fragment>
   );
