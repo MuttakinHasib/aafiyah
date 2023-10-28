4be359aae4b97f2096e66bbd3a02d33b09c64a1b
Muttakin Islam Hasib <muttakinislamhasib@gmail.com>
2023-10-29 01:27:02 +0600

    feat: :hammer: add change password page

--- apps/web/src/app/(account)/dashboard/change-password/page.tsx
+++ apps/web/src/app/(account)/dashboard/change-password/page.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import { Label, Input, Button } from '@aafiyah/ui';
 
-const page = () => {
-  return <div>page</div>;
+export const metadata = {
+  title: 'Change Password',
 };
 
-export default page;
+const ChangePasswordPage = () => {
+  return (
+    <div className="shadow-box bg-white">
+      <h4 className="border-b py-5 px-8 font-medium text-xl">
+        Change Password
+      </h4>
+      <div className="p-8 max-w-md w-full">
+        <div className="space-y-3 pb-8">
+          <div className="space-y-2">
+            <Label htmlFor="current-password">Current Password</Label>
+            <Input
+              id="current-password"
+              type="password"
+              placeholder="********"
+            />
+          </div>
+          <div className="space-y-2">
+            <Label htmlFor="new-password">New Password</Label>
+            <Input id="new-password" type="password" placeholder="********" />
+          </div>
+          <div className="space-y-2">
+            <Label htmlFor="confirm-password">Confirm Password</Label>
+            <Input
+              id="confirm-password"
+              type="password"
+              placeholder="********"
+            />
+          </div>
+        </div>
+        <Button>Save</Button>
+      </div>
+    </div>
+  );
+};
+
+export default ChangePasswordPage;
