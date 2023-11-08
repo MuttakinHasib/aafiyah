6d82937ec9b0ec215737b76a52747184b9ef30bd
Muttakin Islam Hasib <muttakinislamhasib@gmail.com>
2023-11-08 18:06:15 +0600

    feat(web): :hammer: refactor prefetchQuery

--- apps/web/src/app/(account)/dashboard/address-book/page.tsx
+++ apps/web/src/app/(account)/dashboard/address-book/page.tsx
@@ -1,25 +1,9 @@
-import { ADDRESSES_ROUTE, baseURL, getCookieString } from '@aafiyah/client';
-import { QueryClient } from '@tanstack/react-query';
 import AddressBookScreen from '../screens/address-book.screen';
 
 export const metadata = {
   title: 'Address Book',
 };
 
-const AddressBookPage = async () => {
-  const cookie = getCookieString('connect.sid');
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: ['addresses'],
-    queryFn: async () =>
-      await fetch(`${baseURL}${ADDRESSES_ROUTE}`, {
-        credentials: 'include',
-        headers: { cookie },
-      }),
-  });
-
-  return <AddressBookScreen />;
-};
+const AddressBookPage = async () => <AddressBookScreen />;
 
 export default AddressBookPage;
--- apps/web/src/app/(account)/layout.tsx
+++ apps/web/src/app/(account)/layout.tsx
@@ -1,4 +1,4 @@
-import { baseURL, getCookieString } from '@aafiyah/client';
+import { ADDRESSES_ROUTE, baseURL, getCookieString } from '@aafiyah/client';
 import { AccountNavigation } from '@aafiyah/ui';
 import { QueryClient } from '@tanstack/react-query';
 import React, { PropsWithChildren } from 'react';
@@ -7,14 +7,24 @@ const AccountLayout = async (props: PropsWithChildren) => {
   const cookie = getCookieString('connect.sid');
   const queryClient = new QueryClient();
 
-  await queryClient.prefetchQuery({
-    queryKey: ['me'],
-    queryFn: async () =>
-      await fetch(`${baseURL}/users/me`, {
-        credentials: 'include',
-        headers: { cookie },
-      }),
-  });
+  await Promise.all([
+    queryClient.prefetchQuery({
+      queryKey: ['me'],
+      queryFn: async () =>
+        await fetch(`${baseURL}/users/me`, {
+          credentials: 'include',
+          headers: { cookie },
+        }),
+    }),
+    queryClient.prefetchQuery({
+      queryKey: ['addresses'],
+      queryFn: async () =>
+        await fetch(`${baseURL}${ADDRESSES_ROUTE}`, {
+          credentials: 'include',
+          headers: { cookie },
+        }),
+    }),
+  ]);
 
   return (
     <div className="bg-gray-100">
