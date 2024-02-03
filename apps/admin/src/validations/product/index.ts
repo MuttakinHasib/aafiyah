74bac06f41e113c598868aa5b9bfad94bde8a3c5
Muttakin Islam Hasib <muttakinislamhasib@gmail.com>
2024-02-03 16:57:01 +0600

    feat: :hammer: Refactor product validation schema

--- apps/admin/src/validations/product/index.ts
+++ apps/admin/src/validations/product/index.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
 
-export const productSchema = z.object({
-  type: z.enum(["simple", "variant"]),
+export const productType = z.enum(["simple", "variant"]);
+
+const commonSchema = z.object({
   name: z
     .string({ required_error: "Name is required" })
     .min(3, { message: "Name must be at least 3 characters long" })
@@ -9,29 +10,11 @@ export const productSchema = z.object({
   description: z
     .string({ required_error: "Description is required" })
     .min(3, { message: "Description must be at least 3 characters long" }),
-  sku: z.coerce.string().optional(),
-  quantity: z.coerce
-    .number()
-    .transform((v) => (v === 0 ? undefined : v))
-    .optional(),
-  price: z.coerce
-    .number()
-    .transform((v) => (v === 0 ? undefined : v))
-    .optional(),
-  salePrice: z.coerce
-    .number()
-    .transform((v) => (v === 0 ? undefined : v))
-    .optional(),
-  costPrice: z.coerce
-    .number()
-    .transform((v) => (v === 0 ? undefined : v))
-    .optional(),
-  taxPrice: z.coerce
-    .number()
-    .transform((v) => (v === 0 ? undefined : v))
-    .optional(),
+  costPrice: z.coerce.number().nonnegative(),
+  taxPrice: z.coerce.number().nonnegative(),
   categories: z.array(
-    z.string({ required_error: "Category is required" }).min(3).max(255)
+    z.string({ required_error: "Category is required" }).min(3).max(255),
+    { required_error: "Categories is required" }
   ),
   brand: z.string({ required_error: "Brand is required" }).min(3).max(255),
   image: z
@@ -44,8 +27,10 @@ export const productSchema = z.object({
         .url({ message: "Image must be a valid URL" })
     )
     .optional(),
-  status: z.enum(["draft", "published", "archived"]),
-  tags: z.array(z.string()).optional(),
+  status: z.enum(["draft", "published", "archived"], {
+    required_error: "Status is required",
+  }),
+  tags: z.array(z.string(), { required_error: "Tags is required" }),
   dimensions: z.object({
     width: z.coerce
       .number()
@@ -65,6 +50,26 @@ export const productSchema = z.object({
       .optional(),
     unit: z.string().optional(),
   }),
+});
+
+const simple = z.object({
+  type: z.literal(productType.enum.simple),
+  sku: z.coerce
+    .string({ required_error: "SKU is required" })
+    .min(3, { message: "SKU must be at least 3 characters long" }),
+  quantity: z.coerce
+    .number({ required_error: "Quantity is required" })
+    .nonnegative(),
+  price: z.coerce
+    .number({ required_error: "Price is required" })
+    .positive({ message: "Price must be greater than 0" }),
+  salePrice: z.coerce
+    .number({ required_error: "Sale price is required" })
+    .positive({ message: "Sale price must be greater than 0" }),
+});
+
+const variant = z.object({
+  type: z.literal(productType.enum.variant),
   variants: z.array(
     z.object({
       attribute: z.string(),
@@ -75,25 +80,34 @@ export const productSchema = z.object({
   variantsOptions: z.array(
     z.object({
       name: z.string({ required_error: "Name is required" }).min(1),
-      sku: z.coerce.string({ required_error: "SKU is required" }),
+      options: z.string(),
+      sku: z.coerce
+        .string({ required_error: "SKU is required" })
+        .min(3, { message: "SKU must be at least 3 characters long" }),
       quantity: z.coerce
         .number({ required_error: "Quantity is required" })
         .nonnegative(),
       price: z.coerce
         .number({ required_error: "Price is required" })
-        .nonnegative()
-        .optional(),
+        .positive({ message: "Price must be greater than 0" }),
       salePrice: z.coerce
         .number({ required_error: "Sale price is required" })
-        .positive(),
-      costPrice: z.coerce
-        .number()
-        .transform((v) => (v === 0 ? undefined : v))
-        .optional(),
-      taxPrice: z.coerce
-        .number()
-        .transform((v) => (v === 0 ? undefined : v))
-        .optional(),
+        .positive({ message: "Sale price must be greater than 0" }),
     })
   ),
 });
+
+const typeSchema = z.discriminatedUnion("type", [simple, variant]);
+
+export const productSchema = z.intersection(commonSchema, typeSchema);
+
+type CommonSchema = z.infer<typeof commonSchema>;
+type SimpleSchema = z.infer<typeof simple>;
+type VariantSchema = z.infer<typeof variant>;
+
+export interface ProductFormFields
+  extends CommonSchema,
+    Omit<SimpleSchema, "type">,
+    Omit<VariantSchema, "type"> {
+  type: "simple" | "variant";
+}
