d7e12e7687559412297e932520bf360d7ccdb254
Muttakin Islam Hasib <muttakinislamhasib@gmail.com>
2024-02-09 08:37:21 +0600

    Refactor uploadFile and uploadFiles methods to return TImage instead of UploadApiResponse

--- apps/admin/src/services/upload/index.ts
+++ apps/admin/src/services/upload/index.ts
@@ -1,14 +1,14 @@
-import { UploadApiResponse } from "cloudinary";
 import { api } from "@/api";
 import { UPLOAD } from "@/constants";
+import { TImage } from "@/types/image";
 
 export const UPLOAD_API = {
-  uploadFile: async (data: FormData): Promise<UploadApiResponse> =>
+  uploadFile: async (data: FormData): Promise<TImage> =>
     await api.post(UPLOAD + "/file", data, {
       headers: { "Content-Type": "multipart/form-data" },
     }),
 
-  uploadFiles: async (data: FormData): Promise<UploadApiResponse[]> =>
+  uploadFiles: async (data: FormData): Promise<TImage[]> =>
     await api.post(UPLOAD + "/files", data, {
       headers: { "Content-Type": "multipart/form-data" },
     }),
