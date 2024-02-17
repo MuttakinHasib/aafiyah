ca995424fd4713da49d2d4208b4d918275bc8eb4
Muttakin Islam Hasib <muttakinislamhasib@gmail.com>
2024-02-18 02:00:03 +0600

    Add remotePatterns to next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "example.com",
+      },
+      {
+        hostname: "res.cloudinary.com",
+      },
+      {
+        hostname: "www.flaticon.com",
+      },
+      {
+        hostname: "cdn-icons-png.flaticon.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
