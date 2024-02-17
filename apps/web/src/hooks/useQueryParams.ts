299422a9e0f7fc9d316a338be624b32ab3d24f70
Muttakin Islam Hasib <muttakinislamhasib@gmail.com>
2024-02-18 00:50:04 +0600

    Add useQueryParams hook for managing query parameters

---
+++ apps/web/src/hooks/useQueryParams.ts
@@ -0,0 +1,21 @@
+import { buildQuery, getQueries } from "@/utils";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
+
+export const useQueryParams = () => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const setQuery = useCallback(
+    <T = Record<string, string | string[]>>(queries: T) => {
+      const current = getQueries(searchParams);
+      Object.assign(current, queries);
+      const query = buildQuery(current);
+      replace(`${pathname}?${query}`);
+    },
+    [pathname, replace, searchParams]
+  );
+
+  return { setQuery };
+};
