1736ef432d17f03c20d18bebfa822f7ec11fb6ab
Muttakin Islam Hasib <muttakinislamhasib@gmail.com>
2024-02-18 01:25:09 +0600

    Refactor view options and sort functionality

--- apps/web/src/app/(public)/products/components/view-options.component.tsx
+++ apps/web/src/app/(public)/products/components/view-options.component.tsx
@@ -1,48 +1,52 @@
 import { Icons } from "@/components/icons";
-import React from "react";
+import { useProductFilter } from "@/hooks";
+import { cn } from "@/utils";
+import React, { useState } from "react";
+
+const viewOptions = [
+  {
+    id: "grid",
+    icon: Icons.grid,
+  },
+  {
+    id: "column",
+    icon: Icons.column,
+  },
+  {
+    id: "row",
+    icon: Icons.row,
+  },
+  {
+    id: "table",
+    icon: Icons.table,
+  },
+];
 
 export const ProductViewOptions = () => {
+  const [view, setView] = useState("grid");
+
+  const { filter, handleSortChange } = useProductFilter();
   return (
     <div className="py-3 px-4 shadow-box bg-white">
       <div className="flex items-center justify-between">
         <div className="flex items-center space-x-5">
           <div className="flex items-center">
-            <button
-              type="button"
-              className="p-1 hover:bg-gray-100 transition duration-300 group"
-            >
-              <Icons.grid
-                className="stroke-white fill-gray-600 group-hover:fill-brand"
-                size={25}
-              />
-            </button>
-            <button
-              type="button"
-              className="p-1 hover:bg-gray-100 transition duration-300 group"
-            >
-              <Icons.column
-                className="stroke-white fill-gray-600 group-hover:fill-brand"
-                size={25}
-              />
-            </button>
-            <button
-              type="button"
-              className="p-1 hover:bg-gray-100 transition duration-300 group"
-            >
-              <Icons.row
-                className="stroke-white fill-gray-600 group-hover:fill-brand"
-                size={25}
-              />
-            </button>
-            <button
-              type="button"
-              className="p-1 hover:bg-gray-100 transition duration-300 group"
-            >
-              <Icons.table
-                className="stroke-white fill-gray-600 group-hover:fill-brand"
-                size={25}
-              />
-            </button>
+            {viewOptions.map((option) => (
+              <button
+                key={option.id}
+                type="button"
+                className="p-1 hover:bg-gray-100 transition duration-300 group"
+                onClick={() => setView(option.id)}
+              >
+                <option.icon
+                  className={cn(
+                    "stroke-white group-hover:fill-brand transition duration-300",
+                    view === option.id ? "fill-brand" : "fill-gray-600"
+                  )}
+                  size={25}
+                />
+              </button>
+            ))}
           </div>
           <p className="text-sm font-normal whitespace-nowrap">
             Showing 1–8 of 24 products
@@ -51,7 +55,12 @@ export const ProductViewOptions = () => {
         <div className="flex items-center space-x-5">
           <div className="flex items-center space-x-2">
             <p className="text-sm font-normal">Sort by:</p>
-            <select className="text-sm font-normal">
+            <select
+              className="text-sm font-normal"
+              value={filter.sort}
+              onChange={(e) => handleSortChange(e.target.value)}
+            >
+              <option value="">Default</option>
               <option value="featured">Featured</option>
               <option value="best-selling">Best Selling</option>
               <option value="price-ascending">Price: Low to High</option>
