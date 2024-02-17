b69cd2ff54a4b454053cbbcc25f703c85e7049da
Muttakin Islam Hasib <muttakinislamhasib@gmail.com>
2024-02-18 02:00:12 +0600

    Add metadata and update page component

--- apps/web/src/app/(public)/products/page.tsx
+++ apps/web/src/app/(public)/products/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import ProductsScreen from "./screens/products.screen";
+import { Metadata } from "next";
 
-const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+export const metadata: Metadata = {
+  title: "Products",
+  description: "Browse our wide range of products and find what you need.",
 };
+
+const ProductsPage = () => <ProductsScreen />;
 
 export default ProductsPage;
